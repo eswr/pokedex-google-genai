934e9b5b2a05b265b260321c6d1e8379ad5c141c
Koka Easwarendra <easwarendra.ece@gmail.com>
2025-04-08 22:56:04 +0530

    skip sleep

--- src/helpers/sleep.ts
+++ src/helpers/sleep.ts
@@ -1,3 +1,3 @@
-export const sleep = (ms: number) => {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+export const sleep = (_ms: number) => {
+  // return new Promise((resolve) => setTimeout(resolve, ms));
 };
